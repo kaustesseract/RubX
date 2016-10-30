8f50ce0eba700e625b474e30d837027b29efa3e0
KAUSTUBH <kaustubhironmaiden@gmail.com>
2016-10-30 19:53:37 +0530

    initial

---
+++ app/src/main/java/com/kaustubh/rubrics/Showclass.java
@@ -0,0 +1,13 @@
+package com.kaustubh.rubrics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Showclass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_showclass);
+    }
+}
