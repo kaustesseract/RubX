32f3b10967f94de56dc5364137f372e90a3b7399
KAUSTUBH <kaustubhironmaiden@gmail.com>
2017-01-28 21:45:12 +0530

    error solved

--- app/src/main/java/com/kaustubh/rubrics/AddCourse.java
+++ app/src/main/java/com/kaustubh/rubrics/AddCourse.java
@@ -6,18 +6,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class AddCourse extends AppCompatActivity {
 
+    String selection;
+    String clas;
+
     DatabaseHelper helper = new DatabaseHelper(this);
-     private Context context;
+    private Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,47 +31,46 @@ public class AddCourse extends AppCompatActivity {
         setContentView(R.layout.activity_add_course);
 
         ArrayList<String> list = helper.getspinnerdata();
-        Spinner sp = (Spinner)findViewById(R.id.spinner);
-        ArrayAdapter<String> adp= new ArrayAdapter<String>(AddCourse.this,R.layout.spinner_layout,R.id.txt,list);
+        Spinner sp = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> adp = new ArrayAdapter<String>(AddCourse.this, R.layout.spinner_layout, R.id.txt, list);
         sp.setAdapter(adp);
-         String clas = sp.getSelectedItem().toString();
-        final String yo = clas;
-       // Toast.makeText(getApplicationContext(), "Id is "+clas , Toast.LENGTH_LONG).show();
+
+        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            public void onItemSelected(AdapterView<?> parent, View v, int pos, long id) {
+                clas = parent.getItemAtPosition(pos).toString();
+            }
+
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
 
-
-
-        Button bt = (Button)findViewById(R.id.submit);
+        Button bt = (Button) findViewById(R.id.submit);
 
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               /* int clsid = helper.searchcid(clas);
-                EditText cours = (EditText)findViewById(R.id.course);
+
+                // Toast.makeText(getApplicationContext(), "Id is "+selectedItem , Toast.LENGTH_LONG).show();
+                int clsid = helper.searchcid(clas);
+                EditText cours = (EditText) findViewById(R.id.course);
                 String course = cours.getText().toString();
 
                 Contactcourse cr = new Contactcourse();
                 cr.setCourse(course);
                 cr.setClasid(clsid);
                 helper.insertcourse(cr);
-                Toast.makeText(getApplicationContext(), "Course created successfully" , Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Course created successfully", Toast.LENGTH_LONG).show();
                 finish();
-                */
-                Toast.makeText(getApplicationContext(), "Id is "+yo , Toast.LENGTH_LONG).show();
-
-
-
-
-
 
 
             }
         });
 
 
-
-
-
-
     }
 }
+
+
